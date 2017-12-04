5b9c8f1901058ce86599ccaf66be82e76178356b
Christina Collura <cmcollura@gmail.com>
2017-12-04 23:45:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 var recipes = {};
 
 function updateObjectWithKeyAndValue(recipes, key, value){
-  recipes[key] = value;
-  return recipes;
-}
-
-function updateObjectWithKeyAndValue(recipes, key, value){
   var newRecipe = {};
   return Object.assign(newRecipe, recipes, {[key]: value});
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(recipes, key, value){
+  recipes[key] = value;
+  return recipes;
 }
