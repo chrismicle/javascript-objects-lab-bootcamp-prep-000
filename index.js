03d1b515ba9b999f853f51b1c240b0699841a640
Christina Collura <cmcollura@gmail.com>
2017-12-04 23:46:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,3 +9,8 @@ function destructivelyUpdateObjectWithKeyAndValue(recipes, key, value){
   recipes[key] = value;
   return recipes;
 }
+
+function deleteFromObjectByKey(recipes, key){
+  delete recipes[key];
+  return recipes;
+}
